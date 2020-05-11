4c5ecd71152fedcd78b18eb075d4bdd5a546e6e0
Felipe Giannazzo <me@felipegiannazzo.com>
2020-05-11 19:37:41 -0400

    add unstyled color slider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import './App.css';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-  console.log(generatePalette(seedColors[1]));
   return (
     <div className="App">
-      <Palette {...seedColors[1]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
