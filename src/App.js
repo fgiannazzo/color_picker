fb3f489e0af512b42d9ef66fbf85b51a460755fd
Felipe Giannazzo <me@felipegiannazzo.com>
2020-05-12 15:16:51 -0400

    palette route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import './App.css';
 import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-      <Route
-        exact
-        path="/palette/:id"
-        render={() => <h1>INDIVIDUAL PALETTE GOES HERE</h1>}
-      ></Route>
-    </Switch>
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
 
-    // <div className="App">
-    //  <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
-  );
+      // <div className="App">
+      //  <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
