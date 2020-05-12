27c1aa355341a916648c0a4f6d7b1fa438f11829
Felipe Giannazzo <me@felipegiannazzo.com>
2020-05-12 14:23:27 -0400

    add snackbar and styled select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Select from '@material-ui/core/Select';
-import { MenuItem } from '@material-ui/core';
+import { Select, Snackbar, MenuItem, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -8,12 +8,16 @@ import './Navbar.css';
 export default class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: 'hex' };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { format: 'hex', open: false };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackBar = this.closeSnackBar.bind(this);
   }
-  handleChange(e) {
-    this.setState({ format: e.target.value });
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
+  }
+  closeSnackBar() {
+    this.setState({ open: false });
   }
   render() {
     const { level, changeLevel } = this.props;
@@ -36,12 +40,34 @@ export default class Navbar extends Component {
           </div>
         </div>
         <div className="Select-container">
-          <Select value={format} onChange={this.handleChange}>
+          <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
             <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          autoHideDuration={2000}
+          message={
+            <span id="message-id">
+              Format changed to: {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{ 'aria-describedby': 'message-id' }}
+          onClose={this.closeSnackBar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackBar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
       </header>
     );
   }
